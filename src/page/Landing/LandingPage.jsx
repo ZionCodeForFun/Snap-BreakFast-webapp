b07816f29b0041e2485cde07d9d3e3e9c7c5c909
Ameh Emmanuel ZCodes <deanzion76@gmail.com>
2025-10-01 05:26:13 +0100

    just changed a name

--- src/page/Landing/LandingPage.jsx
+++ src/page/Landing/LandingPage.jsx
@@ -1,7 +1,7 @@
 import ServicesSection from "../../components/ServicesSection";
 import CarouselSection from "../../components/CarouselSection";
 import { Container, Main_holder } from "../../style/LandingPageStyle";
-import Faq from "../../components/FAQ";
+import Faq from '../../components/Faq'
 import ProductDetails from "../../components/pop/ProductDetails";
 
 const LandingPage = ({ onLocationInputClick, onLiveLocation }) => {
